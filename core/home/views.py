3715b5a7643c03439e2f2908db13583d616d2de2
Aneesh02 <aneeshtickoo2002@gmail.com>
2023-10-31 16:44:24 +0530

    Added basic backend file

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout, login
+from django.http import HttpResponse
+from home.models import Photo, Person, PersonGallery
+import re
+# ReGEx required for getting photo name
+post_type = re.compile(r"static/images/(.*)")
 # Create your views here.
+def landing(request):
+    return render(request, "landing.html")
+def loginUser(request):
+    pass
+
+def index(request):
+    pass
+
+def loginUser(request):
+    pass
+
+def logoutUser(request):
+    pass
+
+def registerUser(request):
+    pass
+
+def registerUser2(request):
+    pass
+
+def process(request):
+    pass
