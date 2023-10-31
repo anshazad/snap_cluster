291734cb8704a2d28aff27f32c6e128f672e29cf
Aneesh02 <aneeshtickoo2002@gmail.com>
2023-10-31 16:16:01 +0530

    Added the models and database schema

--- core/home/admin.py
+++ core/home/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from .models import Photo, Person, PersonGallery
 
+# Register your models here.
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("home.urls")),
-]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+admin.site.register(Photo)
+admin.site.register(Person)
+admin.site.register(PersonGallery)
--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Photo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(null=False, blank=False)
+
+    def __str__(self):
+        return self.user
+
+
+class Person(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    thumbnail = models.ImageField(null=False, blank=False)
+
+    def __str__(self):
+        return self.user
+
+
+class PersonGallery(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    image = models.ImageField(null=False, blank=False)
+
+    def __str__(self):
+        return self.person.user
