17d02d90e99e44d9a2e03dd044c6e8b044ac7378
Vishesh Thakur <thakurvishesh1@gmail.com>
2023-10-31 17:01:39 +0530

    added imports

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout, login
+from django.http import HttpResponse
+from home.models import Photo, Person, PersonGallery
+import re
+import string
+import random
 
-# Create your views here.
+# ReGEx required for getting photo name
+post_type = re.compile(r"static/images/(.*)")
+
+# Required for image processing
+import face_recognition
+import cv2
+from sklearn.cluster import DBSCAN
+import numpy as np
+import re
+
+# Required for downloading
+import os
+import zipfile
+import tempfile, zipfile
+from django.http import HttpResponse
+from wsgiref.util import FileWrapper
+
+# for checking face simmilarity
+import scipy.spatial.distance as dist
